41dedc53e00e95176c8b9ad3a1ecc5cc100d954d
PonPon <steeevie22@gmail.com>
2023-07-06 11:32:19 -0700

    Optimise conversion to use bit shifts, add debug

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define DEBUG 1
+
+#if DEBUG == 1
+#include <stdlib.h>
+#include <time.h>
+#endif
+
 typedef struct RGBData {
         uint8_t R;
         uint8_t G;
@@ -23,23 +30,37 @@ extern inline void postConversionProcessing( void ){
 
 extern inline yccdata convertRGBtoYCC(rgbdata RGB){
     yccdata YCC;
-    register uint8_t Y, Cb, Cr;
+    register uint8_t Y, Cb, Cr, R, G, B;
 
-    Y  = (uint8_t)(16  + (uint16_t)(( 65.738 * RGB.R) + (129.057 * RGB.G) + ( 25.064 * RGB.B)) >> 8);
-    Cb = (uint8_t)(128 - (uint16_t)(( 37.945 * RGB.R) - ( 74.494 * RGB.G) + (112.439 * RGB.B)) >> 8);
-    Cr = (uint8_t)(128 + (uint16_t)((112.439 * RGB.R) + ( 94.154 * RGB.G) + ( 18.285 * RGB.B)) >> 8);
+    R = RGB.R;
+    G = RGB.G;
+    B = RGB.B;
 
-    return YCC = (yccdata){.Y=Y, .Cr=Cr, .Cb=Cb};
+    Y  = (uint8_t)(16  + (((R << 6) + (R << 1) + (G << 7) + G + (B << 4) + (B << 3) + B) >> 8));
+    Cb = (uint8_t)(128 + (( -((R << 5) + (R << 2) + (R << 1)) - ((G << 6) + (G << 3) + (G << 1)) + (B << 7) - (B << 4)) >> 8));
+    Cr = (uint8_t)(128 + (((R << 7) - (R << 4) - ((G << 6) + (G << 5) - (G << 1)) - ((B << 4) + (B << 1))) >> 8));
+
+    return YCC = (yccdata){.Y=Y, .Cb=Cb, .Cr=Cr};
 }
 
 extern inline rgbdata obtainPixelValue( void ){
     rgbdata RGB;
-    return RGB = (rgbdata){.R=0, .G=255, .B=0};
+    return RGB = (rgbdata){.R=0, .G=255, .B=0}; // Placeholder
 }
 
 int main(void){
+    rgbdata RGB;
+    #if DEBUG == 1
+    srand(time(NULL));
+    RGB = (rgbdata){
+        .R = rand()%256,
+        .G = rand()%256,
+        .B = rand()%256
+    };
+    #else
+    RGB = obtainPixelValue();
+    #endif
 
-    rgbdata RGB = (rgbdata){.R=0, .G=255, .B=0};
 
     yccdata YCC = convertRGBtoYCC(RGB);
 
